b7c4ec20a26a0285371073ef30c093384417af38
ijp <ijp@CIS-RDS4>
2020-04-24 15:02:27 -0500

    made some tests

---
+++ DataTests/PropetryChangedTests/EntreeTests/AngryChickenPropertyChangedTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using CowboyCafe.Data;
+using System.ComponentModel;
+
+namespace CowboyCafe.DataTests.PropetryChangedTests.EntreeTests
+{
+    public class AngryChickenPropertyChangedTests
+    {
+        /// <summary>
+        /// tests if the angrychicken implements INotifyPropertyChanged
+        /// </summary>
+        [Fact]
+        public void AngryChickenShouldImplementINotifyPropertyChanged()
+        {
+            var sammich = new AngryChicken();
+            Assert.IsAssignableFrom<INotifyPropertyChanged>(sammich);
+        }
+    }
+}
